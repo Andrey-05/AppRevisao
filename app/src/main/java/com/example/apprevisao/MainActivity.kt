643f0e0b16f1c5a0c7cfb6317c0e07c8425988d4
Andrey Rodrigues <andreylrodrigues@hotmail.com>
2022-05-02 22:38:38 -0300

    input fix

--- app/src/main/java/com/example/apprevisao/MainActivity.kt
+++ app/src/main/java/com/example/apprevisao/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.core.text.isDigitsOnly
 
 
 class MainActivity : AppCompatActivity() {
@@ -21,19 +22,25 @@ class MainActivity : AppCompatActivity() {
         val num1 = findViewById<EditText>(R.id.val1)
         val num2 = findViewById<EditText>(R.id.val2)
 
-        val valor1 = Integer.parseInt(num1.text.toString())
-        val valor2 = Integer.parseInt(num2.text.toString())
+        if(num1.text.isNotEmpty() && num2.text.isNotBlank() && num1.text.isDigitsOnly() && num2.text.isDigitsOnly()){
 
-        when{
-            valor1>valor2 -> {
-                result.text = String.format("%d é maior que %d",valor1, valor2)
+            val valor1 = Integer.parseInt(num1.text.toString())
+            val valor2 = Integer.parseInt(num2.text.toString())
+
+            when{
+                valor1 > valor2 -> {
+                    result.text = String.format("%d é maior que %d",valor1, valor2)
+                }
+                valor1 < valor2 -> {
+                    result.text = String.format("%d é menor que %d",valor1, valor2)
+                }
+                else->{
+                    result.text = String.format("%d é igual a %d",valor1, valor2)
+                }
             }
-            valor1<valor2 -> {
-                result.text = String.format("%d é menor que %d",valor1, valor2)
-            }
-            else->{
-                result.text = String.format("%d é igual a %d",valor1, valor2)
-            }
+        }
+        else{
+            result.text = "Precisa preencher os campos"
         }
     }
 }
